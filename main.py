e51e477cb432756e23aeb4eb6f6d2d8b4709f52d
dechoog <dechoog@gmail.com>
2025-02-08 21:30:53 -0500

    A: Agregados modulos Python de Pygame. Primer Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+from constants import *
+def main():
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+	print("Starting asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(F"Screen height: {SCREEN_HEIGHT}")
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		screen.fill((0,0,0))
+if __name__ == "__main__":
+	main()
