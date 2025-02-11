99a09daa31f1d9609ae979b39f0b2e11db164cb8
dechoog <dechoog@gmail.com>
2025-02-10 23:35:00 -0500

    B: Segundo commit del dia

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ def main():
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(F"Screen height: {SCREEN_HEIGHT}")
+	clock  = pygame.time.Clock()
+	dt = 0
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0,0,0))
+		tp = clock.tick(60)
+		dt = tp/1000
 if __name__ == "__main__":
 	main()
